9dd11305a7d9c251c783a96fb6d83a518824eb29
Erick Vieira <vieiraerick666@gmail.com>
2024-04-03 20:01:24 -0300

    Add files via upload

--- Amigo.java
+++ Amigo.java
@@ -1,12 +1,14 @@
 public class Amigo {
     private long telefone;
     private String nome;
+    private static String endereco;
     private LocalDate dataNascimento;
 
-    public Amigo(String nome, long telefone, LocalDate dataNascimento) {
+    public Amigo(String nome, long telefone, LocalDate dataNascimento, String endereco) {
         this.nome = nome;
         this.telefone = telefone;
         this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
     }
 
     public long getTelefone() {
@@ -19,5 +21,9 @@ public class Amigo {
 
     public LocalDate getDataNascimento() {
         return dataNascimento;
+    }
+
+    public static String getEndereco() {
+        return endereco;
     }
 }
